1b40f91a753b9a10d17381b95d8dab7f1eac2412
Sung Lee <logicalschema@users.noreply.github.com>
2020-01-03 09:52:02 -0500

    Update week1.sql
    Solution for Problem 2

--- week1.sql
+++ week1.sql
@@ -14,3 +14,40 @@ The following is the Github assignment for Week 1 for Sung Lee
 */
 
 
+--1 
+SELECT origin, dest, MAX(distance) FROM flights;
+/*
+The destination that is the furthest away is IAH (George Bush Intercontinental) from EWR (Newark Liberty International) with a distance of 4983
+*/
+
+--2
+--The different number of engines in the planes table is 4: 1, 2, 3, 4
+SELECT DISTINCT(engines) FROM planes ORDER BY engines;
+
+--For each engine, the aircraft with the greatest number of seats per engine can be found with these statements. 
+
+SELECT MAX(seats) FROM planes WHERE engines = 1;
+--The max is 16 for engine 1
+
+SELECT * FROM planes WHERE engines = 1 AND seats = 16;
+--The Dehavilland Otter DHC-3
+
+
+SELECT MAX(seats) FROM planes WHERE engines = 2;
+--The max is 400 for engine 2
+
+SELECT * FROM planes WHERE engines = 2 AND seats = 400 ORDER BY model;
+--The Boeing 777-200, 777-222, 777-224, and 777-232
+
+
+SELECT MAX(seats) FROM planes WHERE engines = 3;
+--The max is 379 for engine 3
+
+SELECT * FROM planes WHERE engines = 3 AND seats = 379 ORDER BY model;
+--The Airbus A330-223
+
+SELECT MAX(seats) FROM planes WHERE engines = 4;
+--The max is 450 for engine 4
+
+SELECT * FROM planes WHERE engines = 4 AND seats = 450 ORDER BY model;
+--The Boeing 747-451
